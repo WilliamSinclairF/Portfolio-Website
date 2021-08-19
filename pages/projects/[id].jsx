c56e533f3ccdb06acc23c78e61c9c92c733eb371
WilliamSinclairF <willzy@outlook.com>
2021-08-19 14:41:01 -0400

    fixed linting error (missing key prop)

--- pages/projects/[id].jsx
+++ pages/projects/[id].jsx
@@ -51,7 +51,9 @@ const ProjectDetailPage = ({ project }) => {
               alt="Project image"
             />
             {project.paragraphs ? (
-              project.paragraphs.map((paragraph) => <SectionText>{paragraph}</SectionText>)
+              project.paragraphs.map((paragraph) => (
+                <SectionText key={`${paragraph[0]}${paragraph[1]}${paragraph[2]}`}>{paragraph}</SectionText>
+              ))
             ) : (
               <SectionText>{project.description}</SectionText>
             )}
